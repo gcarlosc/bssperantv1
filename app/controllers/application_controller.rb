da1336cc796aaaba4aff54afd6602fb0eed214e4
Giancarlo Solís <gsoliscastillo@gmail.com>
2022-09-29 23:41:19 -0500

    add command

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :set_schema
+
+  def set_schema
+    ActiveRecord::Base.connection.schema_search_path = "#{ENV.fetch("SCHEMA", 'public')},hstore"
+  end
 end
