bc3b83bc450273c7a0c1403413e4ba5ac1d09a16
Giancarlo Solís <gsoliscastillo@gmail.com>
2022-11-18 13:10:03 -0500

    change

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,4 +1,3 @@
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
-# Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
