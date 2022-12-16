fe318fb4c78939263c9ad9b1857acb3aff169782
Giancarlo Solís <gsoliscastillo@gmail.com>
2022-12-16 11:59:02 -0500

    change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,6 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import "./controllers"
-import "@hotwired/turbo-rails"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
