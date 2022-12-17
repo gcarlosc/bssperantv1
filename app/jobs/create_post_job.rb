b4bee29886f09193adcd11a0055404b7d0d34c05
Giancarlo Solís <gsoliscastillo@gmail.com>
2022-12-16 21:57:04 -0500

    c

--- app/jobs/create_post_job.rb
+++ app/jobs/create_post_job.rb
@@ -1,6 +1,6 @@
 class CreatePostJob
   include Sidekiq::Worker
-  sidekiq_options queue: 'cloud66', retry: 1
+  sidekiq_options queue: 'default', retry: 1
 
   def perform
     ActiveRecord::Base.connection.schema_search_path = 'r148581227346de8c711aced4008225, hstore'
