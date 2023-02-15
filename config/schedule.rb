dd1935a81fd3516f33b239a9d6a7f84ca2ecf2cb
Giancarlo Solís <gsoliscastillo@gmail.com>
2023-02-15 11:35:44 -0500

    change

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,10 @@ every 1.minutes do
   rake "scheduler:post_1min"
 end
 
-every 5.minutes do
-  rake "scheduler:post_5min"
-end
+# every 5.minutes do
+#   rake "scheduler:post_5min"
+# end
 
-every 5.minutes do
-  rake "scheduler:post_one_schema['test']"
-end
+# every 5.minutes do
+#   rake "scheduler:post_one_schema['test']"
+# end
